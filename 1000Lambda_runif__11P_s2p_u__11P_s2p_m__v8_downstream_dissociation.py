84ca68a5214a5841f85a0cf6a57fa861ced0f468
lujlab <121745444+lujlab@users.noreply.github.com>
2023-01-02 22:05:49 +0800

    Add files via upload

---
+++ 1000Lambda_runif__11P_s2p_u__11P_s2p_m__v8_downstream_dissociation.py
@@ -0,0 +1,22 @@
+from ICIER_extended_v8 import *
+
+print('start:\t' + str(datetime.datetime.now()), flush=True)
+
+condition_list = []
+
+with open('Lambda_input/num_runif.txt', 'r') as r_in_file:
+    r_in_lines = r_in_file.readlines()
+
+for i in [k / 10 for k in range(0, 11)]:
+    for j in [k / 10 for k in range(0, 11)]:
+        for k in r_in_lines:
+            tmp = default_condition_set.copy()
+            tmp['Lambda'] = float(k.split('\t')[1])
+            tmp['P_s2p_u'] = i
+            tmp['P_s2p_m'] = j
+            condition_list.append(tmp)
+
+
+Sim_multi_condition(condition_list, 'sim_result/1000Lambda_runif__11P_s2p_u__11P_s2p_m__v8_downstream_dissociation.txt')
+
+print('start:\t' + str(datetime.datetime.now()), flush=True)
